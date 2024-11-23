51bcaf5bba4fb6b3fa308aefd8fdee87dda837df
HorkeAnkush96 <horkeankush96@gmail.com>
2024-11-23 00:24:13 -0800

    test 1

--- Integration-Tests/src/test/java/base/BrowserUtility.java
+++ Integration-Tests/src/test/java/base/BrowserUtility.java
@@ -2,4 +2,7 @@ package base;
 
 public class BrowserUtility {
 
+	public static void main(String[] args) {
+		System.out.println("Hyeee");
+	}
 }
---
+++ Integration-Tests/src/test/java/pojos/Atest.java
@@ -0,0 +1,9 @@
+package pojos;
+
+import com.github.fge.jsonschema.main.cli.Main;
+
+public class Atest {
+public static void main(String[] args) {
+	System.out.println("Hi hello");
+}
+}
